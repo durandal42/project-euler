95d29609bea83ee7da0b1c29a3bcb4e5f4db27d9
David Sloan <durandal42@gmail.com>
2015-07-20 16:01:46 -0700

    support even sizes

--- p028.py
+++ p028.py
@@ -22,7 +22,7 @@ def euler028(size):
   data = [[None] * size for i in range(size)]
 
   # start in the middle...
-  i, j = size/2, size/2
+  i, j = (size-1)/2, (size-1)/2
   # moving right...
   di, dj = 0, 1
 
@@ -39,7 +39,7 @@ def euler028(size):
   data[i][j] = size*size
 
   return (sum(data[i][i] + data[i][-i - 1] for i in range(size))
-           - 1)  # don't double-count center
+           - size % 2)  # if size is odd, the center square (1) is in both diagonals.
 
 # Diagonals grow as size**2, and we're summing them, so euler028 is O(size**3).
 # Fit a cubic to the first four data points (we can always fit an exact cubic to 4 points)
@@ -47,7 +47,7 @@ def euler028(size):
 import numpy
 import fractions
 def find_closed_form(f, data, test=[]):
-  coefficients = numpy.polyfit(data, map(f, data), 3)
+  coefficients = numpy.polyfit(data, map(f, data), len(data)-1)
   rational_coefficients = [fractions.Fraction(c).limit_denominator() for c in coefficients]
   print rational_coefficients
   result = lambda size: sum(size ** p * c for p,c in enumerate(reversed(rational_coefficients)))
@@ -55,14 +55,22 @@ def find_closed_form(f, data, test=[]):
     assert result(x) == f(x)
   return result
 
-euler028 = find_closed_form(euler028, [1,3,5,7], [9])
+# The antidiagonal is in a different location depending on the parity of size, so don't expect
+# the same formula to hold for both odd and even sizes.
+euler028_odd = find_closed_form(euler028, [1,3,5,7], [9])
+euler028_even = find_closed_form(euler028, [2,4,6,8], [10])
+euler028 = lambda size: [euler028_even, euler028_odd][size%2](size)
 
 common.assertEquals(101, euler028(5))
 
 common.submit(euler028(1001), expected=669171001)
 
 # Just showing off; given a closed form, these are essentially free to compute:
+print euler028(1000000)
 print euler028(1000001)
+print euler028(1000000000)
 print euler028(1000000001)
+print euler028(1000000000000)
 print euler028(1000000000001)
+print euler028(1000000000000000)
 print euler028(1000000000000001)
